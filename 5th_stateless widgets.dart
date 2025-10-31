3eab1295360be46993b07c7b08603ca53658cf3f
VALLURI RAHUL <vallurirahul3@gmail.com>
2025-10-31 12:13:10 +0530

    Update 5th_stateless widgets.dart

--- 5th_stateless widgets.dart
+++ 5th_stateless widgets.dart
@@ -395,3 +395,52 @@ onPressed: () { context.read<MovieProvider>().removeFromList(currentMovie);
 ); 
 } 
 } run all codes
+State Management using setState(): 
+import 'package:flutter/material.dart'; 
+ 
+void main() { runApp(MyApp()); 
+} 
+ 
+class MyApp extends StatelessWidget { @override 
+Widget build(BuildContext context) { return MaterialApp( 
+home: CounterPage(), 
+); 
+} 
+} 
+ 
+class CounterPage extends StatefulWidget { @override 
+_CounterPageState createState() => _CounterPageState(); 
+} 
+class _CounterPageState extends State<CounterPage> { int _counter = 0; 
+ 
+void _incrementCounter() { setState(() { 
+_counter++; 
+}); 
+} 
+ 
+@override 
+Widget build(BuildContext context) { return Scaffold( 
+appBar: AppBar( 
+title: Text('Counter Example (setState)'), 
+), 
+body: Center( child: 
+ 
+23 
+ 
+Column( 
+mainAxisAlignment: MainAxisAlignment.center,children: 
+<Widget>[ Text( 
+'Counter Value:', 
+), 
+Text( '$_counter', 
+style: Theme.of(context).textTheme.headline4, 
+), 
+], 
+), 
+), 
+floatingActionButton: FloatingActionButton( onPressed: _incrementCounter, 
+tooltip: 'Increment', child: Icon(Icons.add), 
+), 
+); 
+} 
+} add this code and run output
