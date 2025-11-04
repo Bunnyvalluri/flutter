193587fc21aa002a63d2c472f75dd480504a3b2b
VALLURI RAHUL <vallurirahul3@gmail.com>
2025-11-04 12:56:53 +0530

    Update 10th_components.dart

--- 10th_components.dart
+++ 10th_components.dart
@@ -1,1 +1,149 @@
-
+) Write unit tests for UI components 
+Unit tests for UI components in Flutter typically involves using the flutter_test package along 
+with the testpackage for writing tests. 
+Here's how we can write unit tests for the PostCard widget: 
+import 'package:flutter/material.dart'; import 'package:flutter_test/flutter_test 
+.dart'; 
+import 'package:your_app/post_card.dart'; // Import your widget file void main() { 
+testWidgets('PostCard displays title and body', (WidgetTester tester) async { 
+// Build our widget and trigger a frame.await tester.pumpWidget( MaterialApp( 
+home: 
+PostCard( title: 'Test Title', body: 
+'Test Body', 
+), 
+), 
+); 
+// Verify that the title and body are displayed correctly.expect(find.text('Test Title'), 
+findsOneWidget); expect(find.text('Test Body'), findsOneWidget); 
+}); 
+ 
+testWidgets('PostCard widget has correct styling', (WidgetTester tester) async { 
+// Build our widget and trigger a frame.await tester.pumpWidget( MaterialApp( 
+home: PostCard( title: 'Test Title', body: 'Test Body', 
+), 
+), 
+); 
+// Verify that the text styles are applied correctly. final titleText = 
+tester.widget<Text>(find.text('Test Title'));expect(titleText.style.fontSize, 18); 
+expect(titleText.style.fontWeight, FontWeight.bold); 
+ 
+final bodyText = tester.widget<Text>(find.text('Test Body')); expect(bodyText.style.fontSize, 
+16); 
+}); 
+} 
+ 
+ 
+In this test: 
+We use the testWidgets function from the flutter_test package to define our test cases. 
+ 
+In the first test case, we ensure that the PostCard widget correctly displays the provided title and 
+body text. 
+ 
+In the second test case, we verify that the text styles applied to the title and body texts are as 
+expected. We use expect statements to assert that the expected UI elements are found on the 
+screen and that theirproperties match the expected values. 
+lOMoAR cPSD| 37701387 
+ 
+52 
+ 
+Make sure to replace your_app with the appropriate package name where your PostCard widget 
+resides. 
+Run the tests using the following command: 
+flutter test 
+This will execute all the tests defined in your project, including the ones written for the UI 
+components. 
+Make sure to provide different test scenarios and edge cases to ensure comprehensive test 
+coverage for yourUI components. 
+ 
+b) Use flutter's debugging tools to identify and fix issues? 
+demonstrate the use of Flutter's debugging tools, let's consider a scenario where we have a 
+simple counter app, but there's a bug where the counter is not incrementing when the "+" button 
+is pressed. We'll use Flutter's debugging tools to identify and fix this issue. 
+Here's the code for the counter app: import 'package:flutter/material.dart'; 
+void main() 
+{ 
+runApp (MyAp p()); 
+} 
+class MyApp extends StatelessWidget { @override 
+Widget build(BuildContext context) {return MaterialApp( 
+home: CounterApp(), 
+); 
+} 
+} 
+ 
+class CounterApp extends StatefulWidget {@override 
+_CounterAppState createState() => _CounterAppState(); 
+} 
+class _CounterAppState extends State<CounterApp> {int _counter = 0; 
+ 
+void _incrementCounter() { 
+_counter++; 
+} 
+ 
+@override Widget 
+build(BuildContext context) {return Scaffold( appBar: AppBar( 
+title: Text('Counter App'), 
+), 
+body: 
+Center ( child: 
+Colum n( 
+mainAxisAlignment: MainAxisAlignment.center, children: 
+<Widget>[ Text( 
+lOMoAR cPSD| 37701387 
+ 
+53 
+ 
+'Counter:', 
+style: TextStyle(fontSize: 24), 
+), 
+Text( '$_cou nter', 
+style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold), 
+), 
+], 
+), 
+), 
+floatingActionButton: 
+FloatingActionButton(onPressed: 
+_incrementCounter, tooltip: 
+'Increment', child: 
+Icon(Icons.ad d), 
+), 
+); 
+} 
+} 
+ 
+Now, let's use Flutter's debugging tools to identify and fix the issue: 
+Widget Inspector: First, let's inspect the widget tree to see if the "+" button is correctly wired to 
+the 
+_incrementCounter method. We can do this by running the app in debug mode and enabling the 
+widget inspector. You can do this by clicking the "Open DevTools" button in your IDE (Android 
+Studio/IntelliJIDEA or Visual Studio Code) or running the following command in your terminal: 
+flutter run –debug 
+ 
+Once the app is running, click on the "Toggle Widget Inspector" button in the top-right corner of 
+your app. Then, select the FloatingActionButton widget representing the "+" button. Ensure that 
+the onPressedcallback is correctly set to _incrementCounter. 
+Debugging Console: If everything looks fine in the widget inspector, we can add some debug 
+print statements to the _incrementCounter method to see if it's being called when the button is 
+pressed. Modify the 
+_incrementCounter method as follows:void 
+_incrementCounter() { print('Incrementing counter'); 
+_counter++; 
+} 
+lOMoAR cPSD| 37701387 
+ 
+54 
+ 
+Now, run the app again in debug mode and observe the console output when you press the "+" 
+button. If you don't see the "Incrementing counter" message in the console, it means the 
+incrementCounter method is not being called. 
+Breakpoints: As a final step, let's set a breakpoint in the _incrementCounter method and debug 
+the app to see if it's being hit. Add a breakpoint by clicking on the left margin of the 
+_incrementCounter method in your code editor. Then, run the app in debug mode and press the 
+"+" button. The app should pause at the breakpoint, allowing you to inspect the current state and 
+variables. You can step through the code to see if there are any issues with the execution flow. 
+ 
+By using Flutter's debugging tools, you should be able to identify the issue with the counter app 
+and fix it accordingly. In this case, if the debugging process reveals that the _incrementCounter 
+method is not being called, you can double-check the onPressed callback of the 
+FloatingActionButton to ensure it's correctly wired to the _incrementCounter method.
